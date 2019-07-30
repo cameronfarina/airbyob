376cc424bd63b288880a2b42724147eadf3505fd
Mackie <cameronfarina@gmail.com>
2019-07-30 01:53:13 -0400

    Added Login / Signup Modal

--- frontend/components/search/SearchBar.js
+++ frontend/components/search/SearchBar.js
@@ -21,7 +21,6 @@ class Autocomplete extends Component {
     };
   }
 
-  // Event fired when the input value is changed
   onChange = e => {
     const { suggestions } = this.props;
     const userInput = e.currentTarget.value;
@@ -36,7 +35,7 @@ class Autocomplete extends Component {
         singularSuggestions.push(suggestion);
       }
     });
-    // Filter our suggestions that don't contain the user's input
+
     const filteredSuggestions = singularSuggestions.filter(
       suggestion =>
         suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
@@ -50,9 +49,7 @@ class Autocomplete extends Component {
     });
   };
 
-  // Event fired when the user clicks on a suggestion
   onClick = e => {
-    // Update the user input and reset the rest of the state
     this.setState({
       activeSuggestion: 0,
       filteredSuggestions: [],
@@ -61,29 +58,22 @@ class Autocomplete extends Component {
     });
   };
 
-  // Event fired when the user presses a key down
   onKeyDown = e => {
     const { activeSuggestion, filteredSuggestions } = this.state;
 
-    // User pressed the enter key, update the input and close the
-    // suggestions
     if (e.keyCode === 13) {
       this.setState({
         activeSuggestion: 0,
         showSuggestions: false,
         userInput: filteredSuggestions[activeSuggestion]
       });
-    }
-    // User pressed the up arrow, decrement the index
-    else if (e.keyCode === 38) {
+    } else if (e.keyCode === 38) {
       if (activeSuggestion === 0) {
         return;
       }
 
       this.setState({ activeSuggestion: activeSuggestion - 1 });
-    }
-    // User pressed the down arrow, increment the index
-    else if (e.keyCode === 40) {
+    } else if (e.keyCode === 40) {
       if (activeSuggestion - 1 === filteredSuggestions.length) {
         return;
       }
