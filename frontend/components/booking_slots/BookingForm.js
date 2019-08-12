08d4ef635713a78dcc1e837144c63dc3f6b6df35
Mackie <cameronfarina@gmail.com>
2019-08-12 12:26:38 -0400

    Updated booking form scroll

--- frontend/components/booking_slots/BookingForm.js
+++ frontend/components/booking_slots/BookingForm.js
@@ -15,14 +15,33 @@ class BookingForm extends React.Component {
       listing_id: props.listing.id
     };
 
-    window.addEventListener("scroll", () => {
+    function debounce(func, wait, immediate) {
+      var timeout;
+      return function() {
+        var context = this,
+          args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
+
+    const myScroll = debounce(function() {
       let bookingForm = document.getElementById("booking-form-container");
+      console.log("scrolling");
       if (window.scrollY > 450) {
         bookingForm.classList.add("fixed");
       } else {
         bookingForm.classList.remove("fixed");
       }
-    });
+    }, 500);
+
+    window.addEventListener("scroll", myScroll);
 
     this.renderErrors = this.renderErrors.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
